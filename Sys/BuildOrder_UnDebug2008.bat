5cfa7ebf8b3b9a8cddc2a94ae3aeb79e245e6e19
xiangjunchen <jc_xiang@163.com>
2013-11-30 01:08:18 +0800

    bat

---
+++ Sys/BuildOrder_UnDebug2008.bat
@@ -0,0 +1,5 @@
+
+
+devenv ..\TPCommonCtrl\Src\TPCommonWnd\TPCommonWnd2008.vcproj /build "Unicode Debug"
+devenv ..\TPCommonCtrl\Src\TPExtendCtrl\TPExtendCtrl2008.vcproj /build "Unicode Debug"
+rem devenv ..\TPEffect\PlugInSrc\Effect_Soft_1\MvColorCorrection\MvColorCorrection2008.vcproj /build "Unicode Debug"
